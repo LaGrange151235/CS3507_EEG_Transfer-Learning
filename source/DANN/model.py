01ffb141ffd3353b647530e0cc9dccbdfe575fc1
root <zhangjiayi1512@outlook.com>
2023-04-16 11:47:04 +0800

    clean code

--- source/DANN/model.py
+++ source/DANN/model.py
@@ -1,47 +1,55 @@
+import numpy as np
+import torch
+import torch.optim as optim
 import torch.nn as nn
-from functions import ReverseLayerF
+from torch.utils.data import Dataset, DataLoader
 
-import torchsummary
+class DANN(nn.Module):
+    def __init__(self, momentum):
+        super().__init__()
+        self.feature_extractor = nn.Sequential(
+            nn.Linear(310, 256),
+            nn.BatchNorm1d(num_features=256, momentum=momentum),
+            nn.ReLU(),
+            nn.Linear(256, 128),
+            nn.BatchNorm1d(num_features=128, momentum=momentum),
+            nn.Dropout(),
+            nn.ReLU()
+        )
 
-class CNNModel(nn.Module):
-    def __init__(self):
-        super(CNNModel, self).__init__()
+        self.class_classifier = nn.Sequential(
+            nn.Linear(128, 64),
+            nn.BatchNorm1d(num_features=64, momentum=momentum),
+            nn.ReLU(),
+            nn.Dropout(),
+            nn.Linear(64, 32),
+            nn.BatchNorm1d(num_features=32, momentum=momentum),
+            nn.ReLU(),
+            nn.Linear(32, 4),
+            nn.LogSoftmax(dim=1)
+        )
 
-        self.feature = nn.Sequential()
-        self.feature.add_module('f_conv1', nn.Conv2d(5, 64, kernel_size=3, padding=1))
-        self.feature.add_module('f_bn1', nn.BatchNorm2d(64))
-        self.feature.add_module('f_pool1', nn.MaxPool2d(2))
-        self.feature.add_module('f_relu1', nn.ReLU(True))
-        self.feature.add_module('f_conv2', nn.Conv2d(64, 50, kernel_size=3, padding=1))
-        self.feature.add_module('f_bn2', nn.BatchNorm2d(50))
-        self.feature.add_module('f_drop1', nn.Dropout2d())
-        self.feature.add_module('f_pool2', nn.MaxPool2d(2))
-        self.feature.add_module('f_relu2', nn.ReLU(True))
-
-        self.class_classifier = nn.Sequential()
-        self.class_classifier.add_module('c_fc1', nn.Linear(200, 100))
-        self.class_classifier.add_module('c_bn1', nn.BatchNorm1d(100))
-        self.class_classifier.add_module('c_relu1', nn.ReLU(True))
-        self.class_classifier.add_module('c_drop1', nn.Dropout())
-        self.class_classifier.add_module('c_fc2', nn.Linear(100, 4))
-        self.class_classifier.add_module('c_softmax', nn.LogSoftmax(dim=1))
-
-        self.domain_classifier = nn.Sequential()
-        self.domain_classifier.add_module('d_fc1', nn.Linear(200, 100))
-        self.domain_classifier.add_module('d_bn1', nn.BatchNorm1d(100))
-        self.domain_classifier.add_module('d_relu1', nn.ReLU(True))
-        self.domain_classifier.add_module('d_fc2', nn.Linear(100, 2))
-        self.domain_classifier.add_module('d_softmax', nn.LogSoftmax(dim=1))
+        self.domain_classifier = nn.Sequential(
+            nn.Linear(128, 32),
+            nn.BatchNorm1d(32, momentum=momentum),
+            nn.ReLU(),
+            nn.Linear(32, 2),
+            nn.LogSoftmax(dim=1)
+        )
 
     def forward(self, input_data, alpha=0):
-        feature = self.feature(input_data)
-        feature = feature.view(-1, 50 * 2 * 2)
-        reverse_feature = ReverseLayerF.apply(feature, alpha)
-        class_output = self.class_classifier(feature)
-        domain_output = self.domain_classifier(reverse_feature)
-
-        return class_output, domain_output
-
-if __name__=="__main__":
-    model = CNNModel().cuda()
-    torchsummary.summary(model, (5,9,9))
+        feature = self.feature_extractor(input_data)
+        reverse_feature = ReverseLayer.apply(feature, alpha)
+        class_pred = self.class_classifier(feature)
+        domain_pred = self.domain_classifier(reverse_feature)
+        return class_pred, domain_pred
+    
+class ReverseLayer(torch.autograd.Function):
+    @staticmethod
+    def forward(ctx, x, alpha):
+        ctx.alpha = alpha
+        return x.view_as(x)
+    @staticmethod
+    def backward(ctx, grad_outputs):
+        output = grad_outputs.neg() * ctx.alpha
+        return output, None
--- source/DANN/train.py
+++ source/DANN/train.py
@@ -1,9 +1,11 @@
 import numpy as np
 import random
 import sys
+import os
 import torch
 import torch.optim as optim
 from torch.utils.data import Dataset, DataLoader
+import threading
 
 from utils import *
 from model import *
@@ -18,100 +20,158 @@ def print_dataset_info(train_dataloader, test_dataloader):
         break
     print("train_dataset len: %d" % (len(test_dataloader.dataset)))
 
-best_accs = []
-for i in range(15):
-    dataset_path = "./data/SEED-IV_concatenate_reshape/"+str(i)+"/"
+def test(model, test_dataloader, device, alpha=0):
+    model.eval()
+    n_correct = 0
+    n_total = 0
+    accuracy = 0
+    target_iter = iter(test_dataloader)
+    with torch.no_grad():
+        for i in range(len(test_dataloader)):
+            target_data = target_iter._next_data()
+            t_img, t_label = target_data
+            t_img = t_img.to(device)
+            t_label = t_label.long().to(device)
+            batch_size = len(t_label)
+
+            class_output, _ = model(input_data=t_img, alpha=alpha)
+            pred = class_output.data.max(1, keepdim=True)[1]
+            n_correct += pred.eq(t_label.data.view_as(pred)).cpu().sum()
+            n_total += batch_size
+        accuracy = float(n_correct) / n_total
+        return accuracy
+
+def train(id):
+    dataset_path = "./data/SEED-IV_concatenate_unfold/"+str(id)+"/"
     train_data = np.load(dataset_path+"train_data.npy")
     train_label = np.load(dataset_path+"train_label.npy")
     test_data = np.load(dataset_path+"test_data.npy")
     test_label = np.load(dataset_path+"test_label.npy")
 
-    batch_size = 32
+    train_data = torch.from_numpy(train_data)
+    m = train_data.mean(dim = 0, keepdim=True)
+    s = train_data.std(dim=0, unbiased=True, keepdim=True)
+    train_data = (train_data-m)/s
+    train_data = np.array(train_data)
+
+    test_data = torch.from_numpy(test_data)
+    m = test_data.mean(dim = 0, keepdim=True)
+    s = test_data.std(dim=0, unbiased=True, keepdim=True)
+    test_data = (test_data-m)/s
+    test_data = np.array(test_data)
+
+    #print(train_data.shape, train_label.shape, test_data.shape, test_label.shape)
+
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    batch_size = 256
     n_epoch = 100
     lr = 1e-3
+    momentum = 0.5
+    model = DANN(momentum=momentum).to(device)
+    for param in model.parameters():
+        param.requires_grad = True
+    optimizer = optim.Adam(model.parameters(), lr=lr)
+    class_loss = nn.NLLLoss()
+    domain_loss = nn.NLLLoss()
 
     train_dataset = npArraryDataset(train_data, train_label)
     test_dataset = npArraryDataset(test_data, test_label)
     train_dataloader = DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=True)
     test_dataloader = DataLoader(dataset=test_dataset, batch_size=batch_size, shuffle=True)
 
-    print_dataset_info(train_dataloader, test_dataloader)
+    #print_dataset_info(train_dataloader, test_dataloader)
 
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
-    model = CNNModel().to(device)
-    optimizer = optim.Adam(model.parameters(), lr=lr)
-    loss_class = torch.nn.NLLLoss().to(device)
-    loss_domain = torch.nn.NLLLoss().to(device)
-    for param in model.parameters():
-        param.requires_grad = True
-
-    acc_list = []
+    train_acc_list = []
+    test_acc_list = []
 
     for epoch in range(n_epoch):
         len_dataloader = min(len(train_dataloader), len(test_dataloader))
-        data_source_iter = iter(train_dataloader)
-        data_target_iter = iter(test_dataloader)
-
+        source_iter = iter(train_dataloader)
+        target_iter = iter(test_dataloader)
+    
+        model.train()
         for i in range(len_dataloader):
             p = float(i + epoch * len_dataloader) / n_epoch / len_dataloader
-            alpha = 2. / (1. + np.exp(-10 * p)) - 1
+            alpha = 0.5 * (2. / (1. + np.exp(-10 * p)) - 1)
 
-            data_source = data_source_iter._next_data()
-            s_img, s_label = data_source
-            s_img = s_img.to(device)
-            s_label = s_label.long().to(device)
+            sorce_data = source_iter._next_data()
+            s_data, s_label = sorce_data
 
-            model.zero_grad()
+            optimizer.zero_grad()
             batch_size = len(s_label)
-            domain_label = torch.zeros(batch_size).long().to(device)
+            s_domain_label = torch.zeros(batch_size).long()
 
-            class_output, domain_output = model(input_data=s_img, alpha=alpha)
-            err_s_label = loss_class(class_output, s_label)
-            err_s_domain = loss_domain(domain_output, domain_label)
+            s_data = s_data.to(device)
+            s_label = s_label.long().to(device)
+            s_domain_label = s_domain_label.to(device)
 
-            data_target = data_target_iter._next_data()
-            t_img, _ = data_target
-            t_img = t_img.to(device)
-            batch_size = len(t_img)
-            domain_label = torch.ones(batch_size).long().to(device)
+            s_class_pred, s_domain_pred = model(input_data=s_data, alpha=alpha)
+            s_class_loss = class_loss(s_class_pred, s_label)
+            s_domain_loss = domain_loss(s_domain_pred, s_domain_label)
 
-            _, domain_output = model(input_data=t_img, alpha=alpha)
-            err_t_domain = loss_domain(domain_output, domain_label)
 
-            err = err_t_domain + err_s_domain + err_s_label
-            err.backward()
+            
+            target_data = target_iter._next_data()
+            t_data, t_label = target_data
+            
+            batch_size = len(t_data)
+            t_domain_label = torch.ones(batch_size).long()
+
+            t_data = t_data.to(device)
+            t_domain_label = t_domain_label.to(device)
+
+            t_class_pred, t_domain_pred = model(input_data=t_data, alpha=alpha)
+            t_domain_loss = domain_loss(t_domain_pred, t_domain_label)
+
+            loss = s_class_loss + s_domain_loss + t_domain_loss
+            #print("test_session_id: %d, iter_id: %d, s_class_loss: %.4f, s_domain_loss: %.4f, t_domain_loss: %.4f" % (id, i, s_class_loss, s_domain_loss, t_domain_loss))
+            loss.backward()
             optimizer.step()
 
-            #sys.stdout.write("epoch: %d, [iter: %d / all %d], err_s_label: %f, err_s_domain: %f, err_t_domain: %f" % (epoch, i + 1, len_dataloader, err_s_label.data.cpu().numpy(), err_s_domain.data.cpu().numpy(), err_t_domain.data.cpu().item()))
-            #sys.stdout.flush()
-            print("epoch: %d, [iter: %d / all %d], err_s_label: %f, err_s_domain: %f, err_t_domain: %f" % (epoch, i + 1, len_dataloader, err_s_label.data.cpu().numpy(), err_s_domain.data.cpu().numpy(), err_t_domain.data.cpu().item()))
+        train_acc = test(model=model, test_dataloader=train_dataloader, device=device, alpha=alpha)
+        test_acc = test(model=model, test_dataloader=test_dataloader, device=device, alpha=alpha)
+        train_acc_list.append(train_acc)
+        test_acc_list.append(test_acc)
 
-        accuracy = 0
-        correct = 0
-        total = 0
+        save_path = "model_"+str(id)+"_epoch_"+str(epoch)+".pt"
+        torch.save(model, save_path)
 
-        model.eval()
-        data_target_iter = iter(test_dataloader)
-        for i in range(len(test_dataloader)):
-            data_target = data_target_iter._next_data()
-            t_img, t_label = data_target
-            t_img = t_img.to(device)
-            t_label = t_label.to(device)
-            batch_size = len(t_label)
+        print("test_session_id: %d, epoch: %d, train_acc: %.4f, test_acc: %.4f" % (id, epoch, train_acc, test_acc))
+    
+    max_train_acc = max(train_acc_list)
+    max_test_acc = max(test_acc_list)
+    max_train_acc_idx = train_acc_list.index(max_train_acc)
+    max_test_acc_idx = test_acc_list.index(max_test_acc)
+    for i in range(n_epoch):
+        if i==max_test_acc_idx:
+            continue
+        else:
+            command = "rm model_"+str(id)+"_epoch_"+str(i)+".pt"
+            os.system(command)
+    return max_train_acc, max_train_acc_idx, max_test_acc, max_test_acc_idx
 
-            class_output, _ = model(input_data=t_img, alpha=0)
-            pred = class_output.data.max(1, keepdim=True)[1]
-            correct += pred.eq(t_label.data.view_as(pred)).cpu().sum()
-            total += batch_size
+def train_process(i, max_train_acc_list, max_test_acc_list):
+    max_train_acc, max_train_acc_idx, max_test_acc, max_test_acc_idx = train(i)
+    max_train_acc_list.append(max_train_acc)
+    max_test_acc_list.append(max_test_acc)
+    print("test_session_id: %d, max_train_acc: %.4f @ epoch: %d, max_test_acc: %.4f @ epoch: %d" % (i, max_train_acc, max_train_acc_idx, max_test_acc, max_test_acc_idx))
 
-        accuracy = correct * 1.0 / total
-        acc_list.append(accuracy)
-        print("acc: %.4f" % (accuracy))
+if __name__=="__main__":
+    max_train_acc_list = []
+    max_test_acc_list = []
 
-    best_acc = max(acc_list)
-    best_accs.append(best_acc)
-    print("bast acc: %.4f" % (best_acc))
+    for i in range(15):
+        train_process(i, max_train_acc_list, max_test_acc_list)
 
-avg_best_acc = sum(best_accs)/15.0
-print("avg best acc: %.4f" % (avg_best_acc))
+    #thread_list = []
+    #for i in range(15):
+    #    t = threading.Thread(target=train_process, args=(i, max_train_acc_list, max_test_acc_list, ))
+    #    thread_list.append(t)
+    #for t in thread_list:
+    #    t.start()
+    #for t in thread_list:
+    #    t.join()
+
+    avg_train_acc = float(sum(max_train_acc_list)) / len(max_train_acc_list)
+    avg_test_acc = float(sum(max_test_acc_list)) / len(max_test_acc_list)
+    print("avg_trian_acc: %.4f, avg_test_acc: %.4f" % (avg_train_acc, avg_test_acc))
